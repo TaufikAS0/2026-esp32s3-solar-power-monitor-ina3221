3203632da5ba50575d1d16d53a391350d61466f7
Taufik Angga S <133970407+TaufikAS0@users.noreply.github.com>
2026-07-07 19:23:02 +0700

    feat: add moving history grid window

--- ESP32-SolarPower-CP/firmware_version.h
+++ ESP32-SolarPower-CP/firmware_version.h
@@ -3,8 +3,8 @@
 namespace FirmwareInfo {
 
 // Single source of truth for firmware identity.
-static constexpr char kVersion[] = "v0.13.2";
-static constexpr char kReleaseLabel[] = "browser-history-offload";
+static constexpr char kVersion[] = "v0.14.0";
+static constexpr char kReleaseLabel[] = "moving-grid-window";
 static constexpr char kBuildDate[] = __DATE__;
 static constexpr char kBuildTime[] = __TIME__;
 static constexpr char kBuildStamp[] = __DATE__ " " __TIME__;
