cc12af8e26226b19ce6ed4308e09c1e73592d9f5
Taufik Angga S <133970407+TaufikAS0@users.noreply.github.com>
2026-07-07 22:19:56 +0700

    fix live logs zoom interaction

--- ESP32-SolarPower-CP/firmware_version.h
+++ ESP32-SolarPower-CP/firmware_version.h
@@ -3,8 +3,8 @@
 namespace FirmwareInfo {
 
 // Single source of truth for firmware identity.
-static constexpr char kVersion[] = "v0.16.0";
-static constexpr char kReleaseLabel[] = "browser-zoom-ylock";
+static constexpr char kVersion[] = "v0.16.1";
+static constexpr char kReleaseLabel[] = "live-logs-zoom-fixes";
 static constexpr char kBuildDate[] = __DATE__;
 static constexpr char kBuildTime[] = __TIME__;
 static constexpr char kBuildStamp[] = __DATE__ " " __TIME__;
